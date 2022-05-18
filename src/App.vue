754b442117e96975bb9d8e4be6c193c717cd385d
Vitaliy Koveshnikov <kove.vit@gmail.com>
2022-05-18 17:55:44 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="openModal">Open modal</button>
+    
+    <HelloWorld 
+      :isOpenModal="isOpenModal"
+      @close="isOpenModal = false"
+    />
   </div>
 </template>
 
@@ -12,17 +16,32 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data() {
+    return {
+      isOpenModal: false
+    }
+  },
+
+  methods: {
+    openModal() {
+      this.isOpenModal = true
+    }
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  position: relative;
 }
 </style>
